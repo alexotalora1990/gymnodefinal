6bb06ca04d3fd6b3da824f6612eaca142cd67987
Anamile23 <amacuna@soy.sena.edu.co>
2024-08-06 16:27:30 -0500

    creacion api compras y api proveedores by anamilena

--- helpers/compras.js
+++ helpers/compras.js
@@ -1,7 +1,12 @@
-import compras from "../models/compras.js";
+import Compras from "../models/compras.js";
 
 const helpersCompras={
-    
+    validarExistaId:async (id)=>{
+        const existe = await Compras.findById(id)
+        if (existe==undefined){
+            throw new Error ("Id no existe")
+        }
+    },
 
 }
 
